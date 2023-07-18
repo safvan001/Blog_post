2a67a3b2fd9b1c7e322a17f80546f37b8fc36d89
MOHAMMED SAFVAN M V <126167551+safvan001@users.noreply.github.com>
2023-07-18 15:36:19 +0530

    Update models.py

--- Blog/blog_post/models.py
+++ Blog/blog_post/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 class BlogPost(models.Model):
      CHOICES = (
-        ('sun', 'sunday'),
-        ('mon', 'monday'),
-        ('tue', 'tuesday'),
+        ('1', 'title'),
+        ('2', 'body'),
+        ('3', 'author'),
     )
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField()
-    description = models.TextField(choices=CHOICES,default='mon')
+    description = models.TextField(choices=CHOICES,default='1')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
